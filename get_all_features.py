f7e1d3da14901cacf8514d3bdce2433316c11799
anapysasi <57515498+anapysasi@users.noreply.github.com>
2021-03-17 19:06:12 -0500

    Update get_all_features.py

--- get_all_features.py
+++ get_all_features.py
@@ -1,104 +1,75 @@
-import pandas as pd
-import feature_engineering as fe
+"""
+This file is used to calculate what are the most important features of the model.
+The importance coefficient of each one is printed in a dataframe at the end of the program.
+This coefficient will be an average of all the stocks.
+"""
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score as acc
-from mlxtend.feature_selection import SequentialFeatureSelector as sfs
+from ta.momentum import RSIIndicator
+from ta.momentum import PercentagePriceOscillator
+from ta.trend import macd
+from ta.momentum import awesome_oscillator
+from ta.others import daily_return
+from ta.volatility import BollingerBands
+from sklearn.linear_model import LogisticRegression
+from matplotlib import pyplot
+import yfinance as yf
+from openpyxl import load_workbook
 
-aapl = pd.read_csv('AAPL.csv')
-adbe = pd.read_csv('ADBE.csv')
-aes = pd.read_csv('AES.csv')
-fb = pd.read_csv('FB.csv')
-hal = pd.read_csv('HAL.csv')
-intc = pd.read_csv('INTC.csv')
-ip = pd.read_csv('IP.csv')
-ldos = pd.read_csv('LDOS.csv')
-pfe = pd.read_csv('PFE.csv')
-stz = pd.read_csv('STZ.csv')
+# You need to choose what day you want to check.
+day = 'YYYY-MM-DD'
 
+data = load_workbook('/Users/anaysasi/Documents/UchicagoCourses/Winter2021/RealTimeIntSystems/Python/FinalProject/SPY500.xlsx')
+data = data['Sheet1']
+data = data.values
+columns = next(data)[0:]
+df = pd.DataFrame(data, columns=columns)
+Symbols = df['Symbol']
+lst = []
 
-def features_df(stocks):
+for k in range(500):
+    if k == 344:  # This index gives an error when fitting the model
+        pass
+    else:
+        print(k, Symbols[k])
+        data = yf.Ticker(Symbols[k])
+        reader = data.history(day, interval='1m')
 
-    stocks['Return'] = round(stocks['Close'] / stocks['Open'] - 1, 3)
-    stocks['Change'] = (stocks.Close - stocks.Close.shift(1)).fillna(0)
-    stocks['Volatility'] = stocks.Close.ewm(21).std()
-    stocks['5min'] = stocks.Close.rolling(5).mean()
-    stocks['10min'] = stocks.Close.rolling(10).mean()
-    stocks['30min'] = stocks.Close.rolling(30).mean()
-    stocks['volume change'] = (stocks.Volume - stocks.Volume.shift(1)).fillna(0)
-    stocks['volume change_pct'] = stocks['volume change']/stocks['Volume']
-    stocks['Momentum'] = fe.momentum(stocks.Close, 3)
-    stocks['RSI'] = fe.rsi(stocks)
-    stocks['MACD'], stocks['MACD_Signal'] = fe.macd(stocks)
-    return stocks
+        y = np.where(reader['Close'].shift(-1) > reader['Close'], 1, -1)[30:]
+        reader['rsi_vals'] = RSIIndicator(close=reader.Close, window=10).rsi()
+        reader['macd_vals'] = macd(reader.Close, window_slow = 26, window_fast = 12)
+        reader['Ppo'] = PercentagePriceOscillator(reader.Close, window_slow = 26, window_fast = 12).ppo()
+        reader['awesome_oscillator'] = awesome_oscillator(reader.High, reader.Low, window1=5, window2=29)
+        reader['daily_log_return'] = daily_return(close=reader.Close)
+        reader['Return'] = round(reader['Close'] / reader['Open'] - 1, 3)
+        reader['Change'] = (reader.Close - reader.Close.shift(1)).fillna(0)
+        reader['Volatility'] = reader.Close.ewm(21).std()
+        reader['min5']= reader.Close.rolling(5).mean()
+        reader['min10'] = reader.Close.rolling(10).mean()
+        reader['min30'] = reader.Close.rolling(30).mean()
+        reader['hband_indicator'] = BollingerBands(close=reader.Close).bollinger_hband_indicator()
+        reader['lband_indicator'] = BollingerBands(close=reader.Close).bollinger_lband_indicator()
+        reader['Corr'] = reader.Close.rolling(window=10).corr(reader['min10'])
 
+        split = int(0.7*len(reader))
+        reader = reader.drop(reader.index[range(30)], axis=0)
+        X = reader[['Open', 'High', 'Low', 'Close', 'rsi_vals', 'macd_vals', 'Ppo', 'awesome_oscillator',
+                    'daily_log_return', 'Return', 'Change', 'Volatility', 'min5', 'min10',
+                    'min30', 'hband_indicator', 'lband_indicator', 'Corr']]
 
-all_stocks = [aapl, adbe, aes, fb, hal, intc, ip, ldos, pfe, stz]
+        # define the model
+        model = LogisticRegression(solver='lbfgs', random_state=0, max_iter=4000)
+        # fit the model
+        model.fit(X, y)
+        # get importance
+        importance = model.coef_[0]
+        # summarize feature importance
+        for i,v in enumerate(importance):
+            if abs(v) > 0.1:
+                lst.append([Symbols[k], X.columns[i], abs(v)])
 
-for index, symbol in enumerate(all_stocks):
-    all_stocks[index] = features_df(symbol)
-
-#fit_x = pd.DataFrame({'Open-Close': self.open_close[30:], 'High-Low': self.high_low[30:],
-#                                  'Volatility': self.Volatility[30:], 'RSI': self.rsi[30:],
-#                                  'Momentum': self.momentum[30:], 'MACD': self.macd[30:], '5min': self.min5[30:],
-#                                  '10min': self.min10[30:], '30min': self.min30[30:], 'Change': self.change[30:],
-#                                  'Return': self._return[30:], 'VolChangePct': self.vol_pct_change[30:]})
-#            fit_y = pd.DataFrame({'Predictor': self.y[30:]})
-featured = []
-for index, symbol in enumerate(all_stocks):
-    symbol['Open-Close'] = symbol['Open'] - symbol['Close']
-    symbol['High-Low'] = symbol['High'] - symbol['Low']
-    symbol['Predictor'] = symbol['Close'].diff().fillna(0)
-    symbol['Predictor'] = symbol['Predictor'].apply(lambda x: 1 if x > 0 else -1)
-    symbol = symbol[30:]
-    del symbol['Open'], symbol['Close'], symbol['High'], symbol['Low'], symbol['Dividends'], symbol['Volume'], symbol[
-        'Stock Splits'], symbol['volume change'], symbol['MACD_Signal'], symbol['Datetime'], symbol['Symbol']
-    featured.append(symbol)
-
-
-
-# Evaluating features
-# Use one of the stocks from all_stocks to check feature importance
-
-df = featured[0]
+df = pd.DataFrame(lst, columns=["Symbol", "Feature", "coefficient"])
+print(len(Symbols))
+df = df.groupby('Feature').mean().reset_index(drop=False)
+df = df.sort_values(by=['coefficient'], ascending=False)
 print(df)
-
-# Train/test split
-X_train, X_test, y_train, y_test = train_test_split(
-    df.values[:, :-1],
-    df.values[:, -1:],
-    test_size=0.25,
-    random_state=42)
-
-y_train=y_train.astype('int')
-y_test=y_test.astype('int')
-y_train = y_train.ravel()
-y_test = y_test.ravel()
-
-
-print('Training dataset shape:', X_train.shape, y_train.shape)
-print('Testing dataset shape:', X_test.shape, y_test.shape)
-
-# Build RF classifier to use in feature selection
-clf = RandomForestClassifier(n_estimators=100, n_jobs=1)
-
-# Build step forward feature selection
-sfs1 = sfs(clf,
-           k_features=5,
-           forward=True,
-           floating=False,
-           verbose=2,
-           scoring='accuracy',
-           cv=5)
-
-# Perform SFFS
-sfs1 = sfs1.fit(X_train, y_train)
-
-# Which features?
-feat_cols = list(sfs1.k_feature_idx_)
-print(feat_cols)
-
-# df.to_csv('featured.csv')
-
