16701187d5755c38dc31d7e9e65471aabc8daf17
anapysasi <aysasi@ucm.es>
2021-10-12 20:18:18 -0500

    Final changes

--- create_one_day_data.py
+++ create_one_day_data.py
@@ -10,21 +10,33 @@ import yfinance as yf
 from openpyxl import load_workbook
 
 
-def create_df(day, save=False, datetime_index=False):
+def create_df(day, save=False, datetime_index=False, list_stocks=None):
     """
     creates a dataframe with the data of the 500 stocks in the S&P 500 by minute for the specified day.
     :param day: The format must be 'YYYY-MM-DD'. You can only choose one day within the last 7 days.
     :param save: Determines if you want to save the data, in that case set True. Default false
     :param datetime_index: if true returns a dataframe with the DateTime column as the index. Default False.
+    :param list_stocks: list of stocks to create the dataframe. Default, no list.
     :return data of that day for the stocks.
     """
-    data = load_workbook('SPY500.xlsx')
-    data = data['Sheet1']
-    data = data.values
-    columns = next(data)[0:]
 
-    df = pd.DataFrame(data, columns=columns)
-    symbols = df['Symbol']
+    if list_stocks is None:
+        data = load_workbook('SPY500.xlsx')
+        data = data['Sheet1']
+        data = data.values
+        columns = next(data)[0:]
+
+        df = pd.DataFrame(data, columns=columns)
+        symbols = list(df['Symbol'])
+    else:
+        symbols = list_stocks
+
+    # yfinance changed the API, so there are some stock that dont have information. We need to delete some symbols
+    deleted_stocks = ['ALXN', 'MXIM', 'VAR', 'LB', 'COG', 'FLIR']
+    for element in deleted_stocks:
+        if element in symbols:
+            print('Deleted', element, '. Symbol may be delisted from yfinance.')
+            symbols.remove(element)
 
     i = 0
     data0 = yf.Ticker(symbols[i])
--- use_case.py
+++ use_case.py
@@ -1,37 +1,50 @@
-################## THIS IS A WORK IN PROGRESS ##################
-
-import trading_strategy2 as ts
-import market_actions_strategy2 as ma
+import pandas as pd
+from random import sample
+import trading_strategy1 as ts1
+import trading_strategy2 as ts2
+import market_actions_strategy1 as ma1
+import market_actions_strategy2 as ma2
 from create_one_day_data import create_df
+from openpyxl import load_workbook
+from datetime import date
 
-list1 = ['AAPL', 'MSFT', 'AMZN', 'FB', 'GOOGL', 'GOOG', 'TSLA', 'BRK-B', 'JPM', 'JNJ', 'V', 'DIS', 'NVDA', 'UNH', 'MA', 'PG', 'PYPL', 'HD', 'BAC', 'INTC', 'CMCSA', 'NFLX', 'XOM', 'VZ', 'ADBE', 'ABT', 'T', 'CRM', 'CVX', 'ABBV', 'AVGO', 'CSCO', 'KO', 'PFE', 'MRK', 'WMT', 'PEP', 'TMO', 'NKE', 'LLY', 'ACN', 'TXN', 'MDT', 'QCOM', 'MCD', 'WFC', 'COST', 'NEE', 'HON', 'DHR', 'UNP', 'BMY', 'C', 'AMGN', 'PM', 'LIN', 'SBUX', 'ORCL', 'CAT', 'LOW', 'MS', 'UPS', 'BA', 'GS', 'GE', 'DE', 'RTX', 'AMAT', 'IBM', 'NOW', 'INTU', 'MU', 'AMD', 'MMM', 'BLK', 'AMT', 'BKNG', 'SCHW', 'TGT', 'CHTR', 'CVS', 'AXP', 'ISRG', 'FIS', 'LRCX', 'LMT', 'MO', 'SYK', 'SPGI', 'TJX', 'GILD', 'TFC', 'MDLZ', 'CI', 'ANTM', 'ADP', 'ATVI', 'ZTS', 'CB', 'PLD']
+number_stocks = 500
+today = str(date.today())
 
-list2 = ['CME', 'PNC', 'COP', 'CSX', 'TMUS', 'BDX', 'USB', 'CCI', 'FISV', 'EL', 'GM', 'CL', 'ILMN', 'NSC', 'DUK', 'ICE', 'FDX', 'TWTR', 'ADSK', 'SO', 'GPN', 'MMC', 'ITW', 'EQIX', 'ADI', 'APD', 'D', 'SHW', 'BSX', 'VRTX', 'COF', 'AON', 'ECL', 'ETN', 'EW', 'EMR', 'PGR', 'HUM', 'FCX', 'KLAC', 'REGN', 'DG', 'F', 'HCA', 'NOC', 'MCO', 'IDXX', 'MET', 'DOW', 'NEM', 'KMB', 'WM', 'TEL', 'BIIB', 'ROST', 'ALGN', 'SYY', 'APTV', 'JCI', 'CMG', 'HPQ', 'MCHP', 'BAX', 'ROP', 'MAR', 'CDNS', 'CTSH', 'SLB', 'EA', 'LHX', 'DXCM', 'GD', 'AIG', 'DD', 'EXC', 'EOG', 'APH', 'DLR', 'CMI', 'SNPS', 'A', 'AEP', 'SPG', 'IQV', 'PH', 'EBAY', 'TRV', 'TT', 'TROW', 'VIAC', 'STZ', 'PSX', 'PSA', 'MPC', 'WBA', 'BK', 'MSCI', 'PRU', 'HLT', 'LUV']
+data = load_workbook('SPY500.xlsx')
+data = data['Sheet1']
+data = data.values
+columns = next(data)[0:]
+df = pd.DataFrame(data, columns=columns)
+Symbols = list(df['Symbol'])
 
-list3 = ['CNC', 'ZBH', 'IFF', 'CTVA', 'GIS', 'INFO', 'SRE', 'ALXN', 'MNST', 'ALL', 'ORLY', 'XLNX', 'PPG', 'PCAR', 'ADM', 'TDG', 'VLO', 'AFL', 'YUM', 'XEL', 'DAL', 'PXD', 'CARR', 'MSI', 'SWKS', 'ANSS', 'GLW', 'PAYX', 'DFS', 'FRC', 'KMI', 'CTAS', 'WLTW', 'SBAC', 'WELL', 'ROK', 'SWK', 'RMD', 'BLL', 'WMB', 'ETSY', 'ES', 'PEG', 'MCK', 'AME', 'AZO', 'VRSK', 'FAST', 'ZBRA', 'MTD', 'LYB', 'KEYS', 'SIVB', 'AMP', 'OTIS', 'AWK', 'STT', 'DHI', 'WEC', 'CBRE', 'VFC', 'WY', 'KR', 'MXIM', 'FITB', 'AVB', 'KHC', 'LH', 'FLT', 'BBY', 'DLTR', 'AJG', 'LEN', 'CLX', 'DTE', 'FTNT', 'CPRT', 'ED', 'CDW', 'EQR', 'OXY', 'FTV', 'VMC', 'ENPH', 'EXPE', 'HSY', 'URI', 'TER', 'TTWO', 'CERN', 'O', 'MKTX', 'SYF', 'MLM', 'MKC', 'WDC', 'WST', 'ODFL', 'LVS', 'EIX']
+lst_stocks = sample(Symbols, number_stocks)
+deleted_stocks = ['ALXN', 'MXIM', 'VAR', 'LB', 'COG', 'FLIR']
+if number_stocks != 500:
+    print('We randomly choose', number_stocks, 'stocks from the SPY 500. \n The chosen stocks are the following:')
+    print(lst_stocks)
 
-list4 = ['PPL', 'QRVO', 'TSN', 'CCL', 'KSU', 'VTR', 'RF', 'NTRS', 'EFX', 'OKE', 'KEY', 'KMX', 'CHD', 'IP', 'VRSN', 'ARE', 'MTB', 'HAL', 'COO', 'HPE', 'RSG', 'CTLT', 'TYL', 'CFG', 'TRMB', 'TSCO', 'TFX', 'HOLX', 'GRMN', 'PAYC', 'ULTA', 'HIG', 'NUE', 'FE', 'XYL', 'HES', 'VTRS', 'DRI', 'DOV', 'AES', 'AEE', 'ETR', 'IR', 'RCL', 'AMCR', 'WAT', 'GWW', 'ALB', 'ESS', 'CAG', 'BKR', 'BR', 'CTXS', 'STX', 'EXR', 'CE', 'VAR', 'IT', 'NDAQ', 'MPWR', 'NVR', 'PEAK', 'HBAN', 'DGX', 'EXPD', 'CMS', 'MGM', 'UAL', 'MAA', 'AKAM', 'ANET', 'GPC', 'CAH', 'J', 'STE', 'ABC', 'EMN', 'OMC', 'IEX', 'K', 'ABMD', 'CINF', 'INCY', 'AVY', 'DRE', 'PFG', 'RJF', 'PKI', 'BXP', 'NTAP', 'MAS', 'TDY', 'DPZ', 'POOL', 'DISCK', 'FMC', 'BF-B', 'HRL', 'WAB', 'WYNN']
-
-list5 = ['AAL', 'SJM', 'LB', 'DVN', 'LYV', 'BIO', 'PKG', 'CHRW', 'JBHT', 'EVRG', 'LUMN', 'UDR', 'PHM', 'HAS', 'WHR', 'LDOS', 'HST', 'FFIV', 'TPR', 'LW', 'PWR', 'XRAY', 'FBHS', 'NLOK', 'TXT', 'LNT', 'WRK', 'JKHY', 'FOXA', 'L', 'LKQ', 'SNA', 'BWA', 'HWM', 'FANG', 'AAP', 'CBOE', 'CNP', 'ATO', 'MHK', 'LNC', 'IPG', 'MOS', 'IRM', 'ALLE', 'WRB', 'UHS', 'CF', 'RE', 'WU', 'CMA', 'NCLH', 'PNR', 'CPB', 'NWSA', 'GL', 'NRG', 'RHI', 'HSIC', 'MRO', 'NWL', 'ZION', 'REG', 'DISCA', 'IVZ', 'TAP', 'NI', 'IPGP', 'ALK', 'AOS', 'NLSN', 'KIM', 'PNW', 'DISH', 'JNPR', 'PBCT', 'DVA', 'APA', 'COG', 'ROL', 'AIZ', 'BEN', 'HII', 'PVH', 'FLIR', 'FRT', 'VNO', 'SEE', 'DXC', 'HBI', 'NOV', 'LEG', 'RL', 'HFC', 'PRGO', 'UNM', 'GPS', 'SLG', 'FOX', 'FLS']
-
-lst_days = ['2021-03-18', '2021-03-17', '2021-03-16', '2021-03-15', '2021-03-14', '2021-03-13', '2021-03-12']
-lst_stocks = list1[:20]
-for i in list2[:20]:
-    lst_stocks.append(i)
-for i in list3[:20]:
-    lst_stocks.append(i)
-for i in list4[:20]:
-    lst_stocks.append(i)
-for i in list5[:20]:
-    lst_stocks.append(i)
-
+for element in deleted_stocks:
+    if element in lst_stocks:
+        print('Deleted', element, '. Symbol may be delisted from yfinance.')
+        lst_stocks.remove(element)
+num_to_select = len(lst_stocks)
 
 if __name__ == '__main__':
 
-    for day in lst_days:
-        reader = create_df(day)
-        average_price = (reader.mean(axis=0, numeric_only=True))['Close']
-        print('The mean price of the stocks on', day, 'is', average_price)
+    reader = create_df(str(today), list_stocks=lst_stocks)
+    average_price = (reader.mean(axis=0, numeric_only=True))['Close']
+    print('\n The mean price of the stocks on', today, 'is', average_price)
+
+    for i in range(2):
+        if i == 0:
+            print('\n STRATEGY 1 (logistic regression)')
+            ts = ts1
+            ma = ma1
+        else:
+            print('STRATEGY 2')
+            ts = ts2
+            ma = ma2
 
         strategy_dic = {}
         market_dic = {}
@@ -41,20 +54,17 @@ if __name__ == '__main__':
         cash = {}
         news = {}
         symbols = lst_stocks
-        num_to_select = 100
-        list_of_random_items = symbols
         counter = 0
 
         length = []
-        for i in range(num_to_select):
-            N = len(reader[reader['Symbol'] == list_of_random_items[i]])
+        for symbol in symbols:
+            N = len(reader[reader['Symbol'] == symbol])
             length.append(N)
-
         N = min(length)
 
         for j in range(N):
-            for i in range(num_to_select):
-                send = reader[reader['Symbol'] == list_of_random_items[i]]
+            for symbol in symbols:
+                send = reader[reader['Symbol'] == symbol]
                 send = send.iloc[j]
                 send = send.to_dict()
 
@@ -71,11 +81,4 @@ if __name__ == '__main__':
                     market_dic['market' + str(send["Symbol"])].action_buy_sell_hold(
                         send, dic['_action' + str(send["Symbol"])])
 
-            if list(news.values()) == [None] * num_to_select:
-                pass
-            else:
-                sum_total = sum(total.values())
-                sum_cash = sum(cash.values())
-                sum_holdings = sum(holdings.values())
-
-        print('You made:', sum(total.values()) - num_to_select * 100000)
+        print('You made: $', sum(total.values()) - num_to_select * 100000)
